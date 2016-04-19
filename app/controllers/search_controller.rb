68d6877d3144c6adecc5b0111b50ca082203a90b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-04-19 08:20:55 +0300

    add requests examples

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,30 @@
 class SearchController < ApplicationController
   def index
   end
+
+  # get all laboratories
+  # PREFIX vivoplus:<http://vivoplus.aksw.org/ontology#>
+  # select distinct * where {
+  #   ?department a vivoplus:Laboratory ;
+  #               a vivo:Laboratory;
+  #               rdfs:label ?name.
+  # }
+
+  # get all people
+  # PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  # PREFIX vivo:<http://vivoweb.org/ontology/core#>
+  # select distinct * where {
+  #   ?person a foaf:Person ;
+  #           foaf:firstName ?firstName;
+  #           vivo:middleName ?middleName;
+  #           foaf:lastName ?lastName.
+  # }
+
+  # get all research area
+  # PREFIX vivoplus:<http://vivoplus.aksw.org/ontology#>
+  # PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+  # select distinct * where {
+  #   ?researchArea a vivoplus:ResearchArea;
+  #                 rdfs:label ?label.
+  # }
 end
